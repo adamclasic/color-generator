042f7e5f6f3a9ba80e6504eb163f5b1136ece6dd
adam allalou <aabou9@gmail.com>
2020-11-10 21:49:16 +0100

    add copytoclipboard

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import { logDOM } from '@testing-library/react';
+import React, {useState} from 'react'
 // import rgbToHex from 'values'
 
 const SingleColor = ({rgb, weight, type}) => {
-  console.log(rgb);
+const [msg, setMsg] = useState(false)
+  // console.log(rgb);
   // var gColor = this.props.gColor
   let rgbValue = rgb.join(',');
   // console.log('----below thi is gColor');
   // console.log('----ontop thi is gColor');
+  const clickHandler = ()=> {
+    setMsg(true)
+    navigator.clipboard.writeText(`rgb(${rgbValue})`)
+    setTimeout(() => {
+      setMsg(false)
+    }, 3000)
+    
+  }
   return (
-    <div className='singleColor' style={{backgroundColor: `rgb(${rgbValue})`}}>
-      <p style={type=="tint" ? {color: 'black'} : {color: 'white'}}>{`%${weight}`}</p>
-      <p style={type=="tint" ? {color: 'black'} : {color: 'white'}}>{`rgb(${rgbValue})`}</p>
+    <div onClick={clickHandler} className='singleColor' style={{backgroundColor: `rgb(${rgbValue})`}}>
+      <p style={type==="tint" ? {color: 'black'} : {color: 'white'}}>{`%${weight}`}</p>
+      <p style={type==="tint" ? {color: 'black'} : {color: 'white'}}>{`rgb(${rgbValue})`}</p>
+      {msg ? <p>copied to clipboard</p> : <p></p>}
+      
     </div>
   )
 }
